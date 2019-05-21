ec6d8f4c084fec9a8bc8098edc55772a40a06445
roberto rocuant <roberto.rocuantv@gmail.com>
2019-05-21 18:22:11 -0400

    sketch with all scripts added and updated

--- contents/01.sketch/sketch.sh
+++ contents/01.sketch/sketch.sh
@@ -18,3 +18,6 @@ sudo cp $(pwd)/arduino/demo/memory.h $(pwd)/arduino/${jsonname}/
 sudo cp $(pwd)/arduino/demo/switches.h $(pwd)/arduino/${jsonname}/
 sudo cp $(pwd)/arduino/demo/wifi.h $(pwd)/arduino/${jsonname}/
 sudo chmod -R 777 $(pwd)/arduino/${jsonname}/
+source activate nouvellie
+python3 $(pwd)/nouvellie/cli_jobs.py wigle_api --wigle_api_name AIDb7eb8cc431ffbbc4862ce116bcb19bf3 --wigle_api_key 3d3ab445c364455f13ab886eef37e0f3 --jobs $(pwd)/data/jobs/wigle_api.csv -f
+python3 $(pwd)/nouvellie/cli_jobs.py arduino --jobs $(pwd)/data/jobs/arduino.csv --sketch arduino/${jsonname}/
